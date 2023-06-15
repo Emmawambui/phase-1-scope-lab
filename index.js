69d2dde4e17f5cb093f4eda5b469816ad1ebe86b
emma wambui <emma.wambui@student.moringaschool.com>
2023-06-15 17:39:16 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+var customerName= 'bob';
+var bestCustomer;
+
+
+function upperCaseCustomerName(){
+  customerName=customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+   bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+     bestCustomer = 'maybe bob'
+}
+ 
+const LeastFavoriteCustomer = 'some initial value';
+
+function changeLeastFavoriteCustomer(){
+     LeastFavoriteCustomer = 'new name'
+}
